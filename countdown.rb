e5c9cfe0dac1cba8bb8534075f218abeea270901
Winston  Chung <18.chung.winston@ctkprep.org>
2019-07-17 23:29:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer>0
+    integer-=1
+    puts "#{integer} SECOND(S)!"
+  if integer == 0
+    puts "HAPPY NEW YEAR!"
 end
